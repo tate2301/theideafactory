77286f7bcd9de56aec6b481c7f3b1a9231eef33f
Kamfeskaya <tatendachris@gmail.com>
2023-07-05 23:55:32 +0200

    fix: add other items

---
+++ src/components/AnimatedCharacter.tsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+
+const sentence = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      staggerChildren: 0.08,
+    },
+  },
+};
+
+const letter = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+export const Anim = () => (
+  <motion.h1
+    className={"text-8xl font-bold text-zinc-950 staggered-animation"}
+    variants={sentence}
+    initial="hidden"
+    animate="visible"
+  >
+    {"Innovate ".split("").map((char, index) => (
+      <AnimatedSpan char={char} />
+    ))}
+    <br />
+    {"Collaborate".split("").map((char, index) => (
+      <AnimatedSpan char={char} />
+    ))}
+    <br />
+    {"Accelerate ".split("").map((char, index) => (
+      <AnimatedSpan char={char} />
+    ))}
+    <br />
+    {"Entrepreneurship. ".split("").map((char, index) => (
+      <AnimatedSpan char={char} />
+    ))}
+  </motion.h1>
+);
+
+function AnimatedSpan({ char }: { char: string }) {
+  return (
+    <>
+      <motion.span variants={letter}>{char}</motion.span>
+    </>
+  );
+}
